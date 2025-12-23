7d42279e45029001dace5514a408e96d387eafaa
ThureinS <94038134+ThureinS@users.noreply.github.com>
2025-12-23 13:37:33 +0630

    Migration, Model, Controller created, db seeder finish and db seeded, event store route started and tested

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Event::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required | string | max:255',
+                'description' => 'nullable | string',
+                'start_at' => 'required | date',
+                'end_at' => 'required | date | after:start_at',
+            ]),
+            'user_id' => 1
+        ]);
+
+        return $event;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event)
+    {
+        return Event::findOrFail($event->id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
